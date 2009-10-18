960c75997a7566a143906a33977da78de92ba80a
Jim Weirich <jim.weirich@gmail.com>
2009-10-18 00:02:11 -0400

    STEP 11 -- Refactor.  Extract method merge_segments.

--- lib/number_to_lcd.rb
+++ lib/number_to_lcd.rb
@@ -72,22 +72,25 @@ class LcdConverter
     ],
   }
 
-  # STEP 10 -- Lots of opportunity for refactoring.  First extract a
-  # digits_segments method.
+  # STEP 11 -- Refactor.  Extract method merge_segments.
 
   def digits_segments(digit)
     SEGMENTS[digit]
   end
   
+  def merge_segments(segment_list)
+    head, *tail = segment_list
+    head.zip(*tail).map { |segs| segs.join(' ') }
+  end
+
   def convert(number)
     if number < 10
       digits_segments(number).map { |seg| "#{seg}\n" }.join
     else
       lo = digits_segments(number % 10)
       hi = digits_segments((number/10) % 10)
-      grouped_segments = hi.zip(lo)
-      joined_segments = grouped_segments.map { |segs| segs.join(' ') }
-      joined_segments.map { |seg| "#{seg}\n" }.join
+      merged_segments = merge_segments([hi, lo])
+      merged_segments.map { |seg| "#{seg}\n" }.join
     end
   end
 end
